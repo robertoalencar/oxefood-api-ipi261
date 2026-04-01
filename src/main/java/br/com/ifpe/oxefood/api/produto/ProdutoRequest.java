07b6197d8039b0f83b3fd7f6946431d067cf59fe
Roberto Alencar <rroberto.alencar@gmail.com>
2026-04-01 16:16:14 -0300

    atualizando projeto

---
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -0,0 +1,42 @@
+package br.com.ifpe.oxefood.api.produto;
+
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoRequest {
+
+    private Long idCategoria;
+
+    private String codigo;
+    
+    private String titulo;
+    
+    private String descricao;
+    
+    private Double valorUnitario;
+    
+    private Integer tempoEntregaMinimo;
+    
+    private Integer tempoEntregaMaximo;
+
+    public Produto build() {
+
+        Produto produto = Produto.builder()
+                .codigo(codigo)
+                .titulo(titulo)
+                .descricao(descricao)
+                .valorUnitario(valorUnitario)
+                .tempoEntregaMinimo(tempoEntregaMinimo)
+                .tempoEntregaMaximo(tempoEntregaMaximo)
+                .build();
+
+        return produto;
+    }
+}
